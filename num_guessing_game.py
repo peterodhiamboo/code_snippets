cb9c9bada8719b21d671032608fc1ba13272351f
peterodhiamboo <peterodhiamboo.gmail.com>
2024-03-21 12:37:37 +0300

    adding a simple guessing script

---
+++ num_guessing_game.py
@@ -0,0 +1,14 @@
+# This is a number guesing game within a specified range:
+# game can be modified by the user inputting the range of number that they desire
+
+def guess_game(last_number_of_range):
+    #Thhhis is used to collect the user input
+    number = int(input('Enter the last number of range: \n range starts from 1 - ? '))
+
+    print(f'You selected : {number}')
+
+    while(number != 1):
+        number = int(input('try again: '))
+
+
+guess_game(3)
