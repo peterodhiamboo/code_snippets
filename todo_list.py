75af3b76283c399574e291f57f46817e7401beda
peterodhiamboo <peterodhiamboo@gmail.com>
2024-03-25 15:01:44 +0300

    delete function incorporated

--- todo_list.py
+++ todo_list.py
@@ -28,7 +28,8 @@ def beginner():
     #storing the data into json file
     #This option cecks if the json file exists and creates one if none exists
 
-    print('Hello, welcome to the CLI todo list app \n please select an option to proceed \n 1. To add tasks \n 2. To update tasks \n 3. To view all stored tasks')
+    print('Hello, welcome to the CLI todo list app \n please select an option to proceed \n 1. To add tasks \n 2. To update tasks \n 3. To view all stored tasks'
+          '\n 4. Delete a task')
     choice_selection = int(input('Enter the selection here : '))
     
     if choice_selection == 1:
@@ -38,6 +39,9 @@ def beginner():
         update_task(unique_identifier)
     elif choice_selection == 3:
         print(view_tasks())
+    elif choice_selection == 4:
+        delete_task_uid = input('UID to task to be deleted : ')
+        delete_task(delete_task_uid)
         
 
 def add_task():
@@ -91,7 +95,29 @@ def update_task(task_uid):
                         indent=4,  
                         separators=(',',': '))
 
-    
+
+def delete_task(uid_del):
+    t_metadata = view_tasks()
+
+    for tsk in t_metadata:
+        if tsk['uid'] == uid_del:
+            print(tsk['uid'] + ' deleted succesfully')
+            tsk.clear() #this erases the dictionary element completely from the memory
+            break
+    else:
+        print('The UID does not match any in our records')
+
+
+    with open('data.json', 'w') as json_file:
+        json.dump(t_metadata, json_file,
+                  indent=4,
+                  separators=(',',': '))
+
+    print(t_metadata)
+
+
+
+
     
 
 def view_tasks():
